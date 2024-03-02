805dd18bbf10271ab287f97c0962593188349f26
jb830 <jbrumfield623@gmail.com>
2024-03-01 16:38:23 -0800

    updated composerAPI

--- routes/brumfield-composer-routes.js
+++ routes/brumfield-composer-routes.js
@@ -155,5 +155,121 @@ router.post('/', async (req, res) => {
     }
 })
 
+/**
+ *  * updateComposerById
+ * @openapi
+ * /api/composers/{id}:
+ *   put:
+ *     tags:
+ *       - Composers
+ *     description:   Update composer by id
+ *     summary: updates and retruns a composer document
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: Composer document id
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: Array of composer documents
+ *       '401':
+ *          description: Invalid composerId
+ *       '500':
+ *         description: Server exception
+ *       '501':
+ *         description: MongoDB Exception
+ */
+router.put('/:id', async (req, res) => {
+    try {
+        //Query the composers collection using the findOne() function and the RequestParams id on the Composer model.
+        const composer = await Composer.findOne({
+            _id: req.params.id
+        }).exec();
+        //if(!composer) Return a status 401 and the specified error message.
+        if (!composer) {
+            res.status(401).send({
+                'message': 'Invalid composerId'
+            });
+            //if (composer) Update the returned composer object by using the set() function 
+            //and mapping the RequestBody fields to the returned objects parameters.
+        } else {
+            composer.set(req.body)
+
+            //Call the save() function on the returned composer object and return the savedComposer object.
+            await composer.save();
+            res.status(200).json(composer);
+        }
+    } catch (e) {
+        if (e) {
+            res.status(501).send({
+                'message': `MongoDB Exception: ${e.message}`
+            });
+        } else if (err) {
+            res.status(500).send({
+                'message': `Server Exception: ${err.message}`
+            });
+        }
+    }
+});
+
+/**
+ *  * deleteComposerById
+ * @openapi
+ * /api/composers/{id}:
+ *   delete:
+ *     tags:
+ *       - Composers
+ *     description:   Deletes composer by id
+ *     summary: deletes a composer document
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: Composer document id
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Composer document
+ *       '500':
+ *         description: Server exception
+ *       '501':
+ *         description: MongoDB Exception
+ */
+router.delete('/:id', async (req, res) => {
+    try {
+        //Call the findByIdAndDelete() function on the Composer model 
+        //and use the RequestParams id as the filter criteria.
+        const composer = await Composer.findByIdAndDelete({
+            _id: req.params.id
+        }).exec();
+        //Either return the deleted composer document or the appropriate message depending on the status code.
+        res.status(200).json(composer);
+
+    } catch (e) {
+        if (e) {
+            res.status(501).send({
+                'message': `MongoDB Exception: ${e.message}`
+            });
+        } else {
+            res.status(500).send({
+                'message': `Server Exception: ${e.message}`
+            });
+        }
+    }
+});
 
 module.exports = router;
